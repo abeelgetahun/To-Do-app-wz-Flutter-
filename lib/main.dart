33a4db4139383e3bfea9c44241e88eec4bf2f6e9
Abel <abelgetahun66@gmail.com>
2025-02-01 22:53:14 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Todo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const WelcomeScreenWrapper(),
-    );
-  }
-}
-
-class WelcomeScreenWrapper extends StatefulWidget {
-  const WelcomeScreenWrapper({Key? key}) : super(key: key);
-
-  @override
-  State<WelcomeScreenWrapper> createState() => _WelcomeScreenWrapperState();
-}
-
-class _WelcomeScreenWrapperState extends State<WelcomeScreenWrapper> {
-  bool? _showWelcomeScreen;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkFirstTime();
-  }
-
-  Future<void> _checkFirstTime() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _showWelcomeScreen = !prefs.getBool('seen_welcome_screen')!;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_showWelcomeScreen == null) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World App'),
+          backgroundColor: Colors.blue,
         ),
-      );
-    }
-
-    return _showWelcomeScreen! ? const WelcomeScreen() : const Scaffold(); // Replace Scaffold() with your main screen
+        body: const Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 }
