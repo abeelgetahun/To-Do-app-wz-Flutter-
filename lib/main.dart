15d7f43c85ff176e9d00fd34313faaee01a72473
Abel <abelgetahun66@gmail.com>
2025-02-02 20:56:47 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome and Counter',
+      title: 'Just do',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
