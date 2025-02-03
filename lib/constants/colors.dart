1d4d9e78263a9379c82c34a77d148042ece6e222
Abel <abelgetahun66@gmail.com>
2025-02-03 22:31:43 +0300

    add color

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+const Color tdRed= Color(0xFFd0c4e3);
+const Color tdBlue= Color(0xFF4e5ae8);
+
+const Color tdGrey= Color(0xFF344955);
+const Color tdDarkGrey= Color(0xFF1F2937);
+
+const Color tdBlack= Color(0xFF000000);
+const Color tdBGColor= Color(0xFFEEEFF5);
+
+
+
+
