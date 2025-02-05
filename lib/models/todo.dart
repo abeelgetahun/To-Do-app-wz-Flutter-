2b6e3ca5e1462e53ce32e5f31a4463c068d21804
Abel <abelgetahun66@gmail.com>
2025-02-05 21:14:09 +0300

    change on data storage method

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,15 +1,33 @@
 class ToDo {
-
   String? id;
   String? todoText;
   bool isDone;
+
   ToDo({
-      required this.id,
-      required this.todoText,
-      this.isDone=false,
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
   });
 
-  static List<ToDo> todoList(){
+  // Convert ToDo to Map
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'todoText': todoText,
+      'isDone': isDone,
+    };
+  }
+
+  // Create ToDo from Map
+  factory ToDo.fromJson(Map<String, dynamic> json) {
+    return ToDo(
+      id: json['id'],
+      todoText: json['todoText'],
+      isDone: json['isDone'],
+    );
+  }
+
+  static List<ToDo> todoList() {
     return [
       ToDo(id: "01", todoText: "check Mail", isDone: true),
       ToDo(id: "02", todoText: "Buy Groceries", isDone: false),
@@ -17,8 +35,6 @@ class ToDo {
       ToDo(id: "04", todoText: "check Mail", isDone: false),
       ToDo(id: "05", todoText: "Team Meeting", isDone: false),
       ToDo(id: "06", todoText: "check import", isDone: true),
-
     ];
   }
-
 }
