880340c5bd212c94149175ac248d04eabdc45ab0
Abel <abelgetahun66@gmail.com>
2025-02-11 19:33:40 +0300

    improve home.dart

--- lib/widget/todo_item.dart
+++ lib/widget/todo_item.dart
@@ -28,7 +28,7 @@ class ToDoItem extends StatelessWidget {
           todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
           color: tdBlue,
         ),
-        //testtt
+        //testttr
         title: Text(
           todo.todoText ?? "", // Ensures no null error
           style: TextStyle(
