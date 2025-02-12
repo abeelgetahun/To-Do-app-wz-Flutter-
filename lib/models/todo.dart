93d31518d5b64b520208de3153902510fd4dfc60
Abel <abelgetahun66@gmail.com>
2025-02-12 19:04:04 +0300

    update

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,7 +1,7 @@
 class ToDo {
-  String? id;
-  String? todoText;
-  bool isDone;
+  final String id;
+  final String todoText;
+  final bool isDone;
 
   ToDo({
     required this.id,
@@ -9,7 +9,7 @@ class ToDo {
     this.isDone = false,
   });
 
-  // Convert ToDo to Map
+  /// Convert ToDo to Map (for JSON serialization)
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -18,23 +18,33 @@ class ToDo {
     };
   }
 
-  // Create ToDo from Map
+  /// Create ToDo from Map (JSON deserialization)
   factory ToDo.fromJson(Map<String, dynamic> json) {
     return ToDo(
-      id: json['id'],
-      todoText: json['todoText'],
-      isDone: json['isDone'],
+      id: json['id'] ?? '',
+      todoText: json['todoText'] ?? '',
+      isDone: json['isDone'] ?? false,
     );
   }
 
-  static List<ToDo> todoList() {
+  /// Creates a copy of the current ToDo with updated fields
+  ToDo copyWith({String? id, String? todoText, bool? isDone}) {
+    return ToDo(
+      id: id ?? this.id,
+      todoText: todoText ?? this.todoText,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+
+  /// Sample ToDo List
+  static List<ToDo> sampleList() {
     return [
-      ToDo(id: "01", todoText: "check Mail", isDone: true),
-      ToDo(id: "02", todoText: "Buy Groceries", isDone: false),
-      ToDo(id: "03", todoText: "Morning Excercise", isDone: false),
-      ToDo(id: "04", todoText: "check Mail", isDone: false),
-      ToDo(id: "05", todoText: "Team Meeting", isDone: false),
-      ToDo(id: "06", todoText: "check import", isDone: true),
+      ToDo(id: "01", todoText: "Check Mail", isDone: true),
+      ToDo(id: "02", todoText: "Buy Groceries"),
+      ToDo(id: "03", todoText: "Morning Exercise"),
+      ToDo(id: "04", todoText: "Check Mail"),
+      ToDo(id: "05", todoText: "Team Meeting"),
+      ToDo(id: "06", todoText: "Check Import", isDone: true),
     ];
   }
 }
