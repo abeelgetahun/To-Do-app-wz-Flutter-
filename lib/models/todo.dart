7732a7e428b3c714a4fca1d2f417e7a06c1b5b1f
Abel <abelgetahun66@gmail.com>
2025-02-13 18:51:13 +0300

    update

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,29 +1,29 @@
-class ToDo {
+import 'package:equatable/equatable.dart';
+
+class ToDo extends Equatable {
   final String id;
   final String todoText;
   final bool isDone;
 
-  ToDo({
+  const ToDo({
     required this.id,
     required this.todoText,
     this.isDone = false,
   });
 
   /// Convert ToDo to Map (for JSON serialization)
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'todoText': todoText,
-      'isDone': isDone,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'todoText': todoText,
+    'isDone': isDone,
+  };
 
   /// Create ToDo from Map (JSON deserialization)
   factory ToDo.fromJson(Map<String, dynamic> json) {
     return ToDo(
-      id: json['id'] ?? '',
-      todoText: json['todoText'] ?? '',
-      isDone: json['isDone'] ?? false,
+      id: json['id'] as String? ?? '',
+      todoText: json['todoText'] as String? ?? '',
+      isDone: json['isDone'] as bool? ?? false,
     );
   }
 
@@ -36,15 +36,21 @@ class ToDo {
     );
   }
 
+  /// Overrides `toString()` for better debugging
+  @override
+  String toString() => 'ToDo(id: $id, todoText: $todoText, isDone: $isDone)';
+
+  /// Equatable override for efficient comparisons
+  @override
+  List<Object?> get props => [id, todoText, isDone];
+
   /// Sample ToDo List
-  static List<ToDo> sampleList() {
-    return [
-      ToDo(id: "01", todoText: "Check Mail", isDone: true),
-      ToDo(id: "02", todoText: "Buy Groceries"),
-      ToDo(id: "03", todoText: "Morning Exercise"),
-      ToDo(id: "04", todoText: "Check Mail"),
-      ToDo(id: "05", todoText: "Team Meeting"),
-      ToDo(id: "06", todoText: "Check Import", isDone: true),
-    ];
-  }
+  static List<ToDo> sampleList() => const [
+    ToDo(id: "01", todoText: "Check Mail", isDone: true),
+    ToDo(id: "02", todoText: "Buy Groceries"),
+    ToDo(id: "03", todoText: "Morning Exercise"),
+    ToDo(id: "04", todoText: "Check Mail"),
+    ToDo(id: "05", todoText: "Team Meeting"),
+    ToDo(id: "06", todoText: "Check Import", isDone: true),
+  ];
 }
